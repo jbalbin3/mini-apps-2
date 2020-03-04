89e8184901e47da3b159eee3f42bb05eca2534f6
John Balbin <jbalbin3@yahoo.com>
2020-03-04 14:16:28 -0800

    basic requirements

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Chart from './Chart.jsx';
+import { TypeChooser } from 'react-stockcharts/lib/helper';
 
 
+const App = () => {
+
+  const [rates, setRates] = React.useState({});
+  const [disclaimer, setDisclaimer] = React.useState('');
+  const [time, setTime] = React.useState('');
+
+  React.useEffect(() => {
+    axios.get('/api/rates')
+      .then((res) => {
+        console.log('DATA',res.data);
+
+        // fix data for charts
+        // [{date: date, close: close},{date: date, close: close}...]
+        let list = Object.entries(res.data.bpi);
+        const entries = list.map((entry)=>{
+          var temp = {};
+          temp.date = new Date(entry[0]);
+          temp.close = entry[1];
+          return temp;
+        });
+
+        console.log('Entries', entries);
+
+        setDisclaimer(res.data.disclaimer);
+        setTime(res.data.time.updated);
+        setRates(entries);
+
+      })
+      .catch((err) => console.log('error fetching rates data', err));
+  }, []);
+
+  if(!rates.length) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+    <h1>Bitcoin Price Index</h1>
+  <h2>{time}</h2>
+  <h3>{disclaimer}</h3>
+
+    <TypeChooser>
+      {type => <Chart type={type} data={rates} />}
+    </TypeChooser>
+    <footer>"Powered by CoinDesk"</footer>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3333;
 
+const axios = require('axios');
+
+
+app.use(express.static('public'));
+app.listen((PORT),()=>{
+  console.log('listening on port',PORT);
+})
+
+app.get('/api/rates', (req,res)=>{
+  // https://api.coindesk.com/v1/bpi/historical/close.json
+  //  Sample Request for date range: https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+    .then((response)=>{
+      console.log(response.data);
+      res.status(200).send(response.data).end();
+    })
+    .catch((err)=>{
+      console.error('error getting json data from coindesk api', err);
+    })
+});
 

5888291ef66801591db6ab73d4838b140dc3f615
John Balbin <jbalbin3@yahoo.com>
2020-03-04 17:34:59 -0800

    advanced content, caching and search for date

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -11,8 +11,46 @@ const App = () => {
   const [disclaimer, setDisclaimer] = React.useState('');
   const [time, setTime] = React.useState('');
 
-  React.useEffect(() => {
-    axios.get('/api/rates')
+  // React.useEffect(() => {
+  //   axios.get('/api/rates/')
+  //     .then((res) => {
+  //       console.log('DATA',res.data);
+
+  //       // fix data for charts
+  //       // [{date: date, close: close},{date: date, close: close}...]
+  //       let list = Object.entries(res.data.bpi);
+  //       const entries = list.map((entry)=>{
+  //         var temp = {};
+  //         temp.date = new Date(entry[0]);
+  //         temp.close = entry[1];
+  //         return temp;
+  //       });
+
+  //       console.log('Entries', entries);
+
+  //       setDisclaimer(res.data.disclaimer);
+  //       setTime(res.data.time.updated);
+  //       setRates(entries);
+
+  //     })
+  //     .catch((err) => console.log('error fetching rates data', err));
+  // }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.date.value);
+
+    var d = new Date(e.target.date.value);
+    const end = d.toLocaleDateString('sv');
+    d.setDate(d.getDate() - 30);
+    const start = d.toLocaleDateString('sv');
+
+    axios.get('/api/rates/',{
+      params: {
+        start: start,
+        end: end
+      }
+    })
       .then((res) => {
         console.log('DATA',res.data);
 
@@ -31,18 +69,33 @@ const App = () => {
         setDisclaimer(res.data.disclaimer);
         setTime(res.data.time.updated);
         setRates(entries);
-
       })
-      .catch((err) => console.log('error fetching rates data', err));
-  }, []);
+      .catch((err)=>{
+        console.error('error receiving list of entries', err);
+      })
+  }
 
   if(!rates.length) {
-    return <div>Loading...</div>
+    return (
+      <div>
+        <h1>Bitcoin Price Index</h1>
+        <h3>Enter a date to get data from date selected to 30 days before.</h3>
+          <form onSubmit={handleSubmit}>
+            <input type="date" name="date"/>
+            <button type="submit">Submit</button>
+          </form>
+      </div>
+    )
   }
 
   return (
     <div>
     <h1>Bitcoin Price Index</h1>
+    <h3>Enter a date to get 30 days of data prior to entered date.</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="date" name="date"/>
+        <button type="submit">Submit</button>
+      </form>
   <h2>{time}</h2>
   <h3>{disclaimer}</h3>
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3333;
-
+const redis = require("redis");
 const axios = require('axios');
+const port_redis = process.env.PORT || 6379;
+const redis_client = redis.createClient(port_redis);
+const bodyParser = require("body-parser");
 
 
 app.use(express.static('public'));
@@ -10,12 +13,37 @@ app.listen((PORT),()=>{
   console.log('listening on port',PORT);
 })
 
-app.get('/api/rates', (req,res)=>{
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Middleware Function to Check Cache
+checkCache = (req, res, next) => {
+  const { end } = req.query;
+
+  redis_client.get(end, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    if (data != null) {
+      console.log('CACHE HIT ON DATE', end);
+      res.send(data);
+    } else {
+      //proceed to next middleware function
+      next();
+    }
+  });
+};
+
+app.get('/api/rates', checkCache, (req,res)=>{
+
+  // console.log('params',  req.query);
   // https://api.coindesk.com/v1/bpi/historical/close.json
   //  Sample Request for date range: https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
-  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+  axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${req.query.start}&end=${req.query.end}`)
     .then((response)=>{
-      console.log(response.data);
+      console.log('NOT CACHED DATA ON DATE', req.query.end);
+      redis_client.setex(req.query.end, 3600, JSON.stringify(response.data));
       res.status(200).send(response.data).end();
     })
     .catch((err)=>{
